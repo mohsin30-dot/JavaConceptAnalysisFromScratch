c209ac817d31e0466cd734ed9f895d575f553192
Mohsin <msis.rubel@gmail.com>
2020-06-21 13:25:58 -0400

    Initial commit

---
+++ src/JavaFundamentals.java
@@ -0,0 +1,31 @@
+/*
+Java interview questions & analysis concept---------------------
+Java language fundamentals:
+1.	Identifiers
+2.	Reserve words
+3.	Data types
+4.	Literals
+5.	Arrays
+6.	Types & variables
+7.	Var-argument methods
+8.	Main method
+9.	Command line arguments
+10.	Java coding standards
+
+Identifiers:
+1.	Identifiers using for identification purpose. A name in java program is by default called Identifier. Which can be used for identification purpose.
+ It’s can be method name, variable name, class name.
+For example:
+class test {
+     public static void main (String[]arg){
+     int x = 10;
+     System-----------
+     how many Identifiers are there?
+      Answer is: 1. test, 2. main 3. String 4. Arg 5. x
+     while defining java identifiers we need to follow some rules--- (a to z) (A to Z) (0 to 9) ($) (-) the only allowed characters in java identifiers
+     are as above, other than any sign will get compile time error.
+     -Identifiers should not start with digit or number.
+     -java identifiers are case sensitive
+     -maximum character allow ? - there is no length limit for java identifiers but for good practice keep in two word.
+
+ */
